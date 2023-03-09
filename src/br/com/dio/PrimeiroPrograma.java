517458f1a69b7ba77f77856efd3c663116639212
Barretotavio <obarretomachado@gmail.com>
2023-03-09 17:20:09 -0300

    create class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,11 +6,47 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
+        System.out.println(gato);
+
+       livro livroh = new livro("The Choise one", 300);
+        System.out.println(livroh);
 
 /*        int a = 5;
         int b = 3;
         System.out.println("Apprehend lingua-gem Java! " + (a+b));*/
+    }
+}
 
+class livro {
+    private String nome;
+    private Integer numPagina;
 
+    public livro(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
